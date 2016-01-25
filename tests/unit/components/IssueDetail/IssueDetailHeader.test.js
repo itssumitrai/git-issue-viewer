5a4f5078dc0e5f71783b311267c197fa3ba8e5a0
Sumit Rai <sumit.rai1987@gmail.com>
2016-01-24 20:56:08 -0800

    More tests

--- tests/unit/components/IssueDetail/IssueDetailHeader.test.js
+++ tests/unit/components/IssueDetail/IssueDetailHeader.test.js
@@ -1,19 +1,76 @@
-/* globals before, beforeEach, it, describe */
+/* globals before, after, beforeEach, it, describe */
 /**
  * Copyright 2016, Sumit Rai
  * Copyrights licensed under the New MIT License. See the accompanying LICENSE file for terms.
  */
 
 'use strict';
-import { expect } from 'chai';
 
-describe('stub', function () {
+import { expect } from 'chai';
+import jsx from 'jsx-test';
+import mockery from 'mockery';
+import reactIntlMock from '../../../mocks/reactIntlMock';
+import issue from '../../../data/issueDetailServiceResponse.json'
+
+describe('IssueDetailHeader', function () {
+    let IssueDetailHeader;
+
     before(function () {
+        mockery.enable({ useCleanCache: true, warnOnUnregistered: false });
+        mockery.registerMock('react-intl', reactIntlMock);
+        mockery.registerMock('../Shared/IssueState', jsx.stubComponent('IssueState', null, true));
+        mockery.registerMock('../Shared/Labels', jsx.stubComponent('Labels', null, true));
+        mockery.registerMock('../Shared/IssueDate', jsx.stubComponent('IssueDate', null, true));
+        IssueDetailHeader = require('../../../../components/IssueDetail/IssueDetailHeader');
     });
 
-    describe('#stub', function () {
-        it('should do something', function () {
-            expect(true).to.be.true;
+    after(function () {
+        mockery.deregisterAll();
+        mockery.disable();
+    });
+
+    describe('#render', function () {
+        let props;
+        beforeEach(function () {
+            props = {
+                issue: issue
+            };
+        });
+
+        it('should render title and issueNumber', function () {
+            jsx.assertRender(IssueDetailHeader, props,
+                '<h2+><span class="title+>*Cannot find module'
+            );
+        });
+
+        it('should render issue state', function () {
+            jsx.assertRender(IssueDetailHeader, props,
+                '<IssueState data-state="open"+></IssueState>'
+            );
+        });
+
+        it('should render user profile url', function () {
+            jsx.assertRender(IssueDetailHeader, props,
+                '<a href="https://github.com/bsdfzzzy"+>bsdfzzzy</a>'
+            );
+        });
+
+        it('should render the date', function () {
+            jsx.assertRender(IssueDetailHeader, props,
+                '<IssueDate *data-datetype="create" data-date="2016-01-20T10:50:02Z"></IssueDate>'
+            );
+        });
+
+        it('should render number of comments', function () {
+            jsx.assertRender(IssueDetailHeader, props,
+                '<span>1 comments</span>'
+            );
+        });
+
+        it('should render issue labels', function () {
+            jsx.assertRender(IssueDetailHeader, props,
+                '<Labels data-labels=""></Labels>'
+            );
         });
     });
 });
--- tests/unit/components/IssueList/PaginationBar.test.js
+++ tests/unit/components/IssueList/PaginationBar.test.js
@@ -1,19 +1,71 @@
-/* globals before, beforeEach, it, describe */
+/* globals before, after, beforeEach, it, describe */
 /**
  * Copyright 2016, Sumit Rai
  * Copyrights licensed under the New MIT License. See the accompanying LICENSE file for terms.
  */
 
 'use strict';
-import { expect } from 'chai';
 
-describe('stub', function () {
+import { expect } from 'chai';
+import jsx from 'jsx-test';
+import mockery from 'mockery';
+import Immutable from 'immutable';
+import { getPageNumbers } from '../../../../lib/utils';
+import reactIntlMock from '../../../mocks/reactIntlMock';
+import fluxibleRouterMock from '../../../mocks/fluxibleRouterMock';
+
+describe('PaginationBar', function () {
+    let PaginationBar;
+
     before(function () {
+        mockery.enable({ useCleanCache: true, warnOnUnregistered: false });
+        mockery.registerMock('react-intl', reactIntlMock);
+        mockery.registerMock('fluxible-router', fluxibleRouterMock);
+        PaginationBar = require('../../../../components/IssueList/PaginationBar');
     });
 
-    describe('#stub', function () {
-        it('should do something', function () {
-            expect(true).to.be.true;
+    after(function () {
+        mockery.deregisterAll();
+        mockery.disable();
+    });
+
+    describe('#render', function () {
+        let props;
+        beforeEach(function () {
+            props = {
+                routeParams: Immutable.fromJS({
+                    owner: 'npm',
+                    repo: 'npm'
+                }),
+                pageNumber: '5',
+                totalPages: 20,
+                pagesToShow: 15
+            };
+        });
+
+        it('should render Previous page link', function () {
+            jsx.assertRender(PaginationBar, props,
+                '<li+><a*href="/npm/npm/4"><span>Previous</span></a></li>'
+            );
+        });
+
+        it('should render Next page Link', function () {
+            jsx.assertRender(PaginationBar, props,
+                '<li+><a href="/npm/npm/6"+><span>Next</span></a></li>'
+            );
+        });
+
+        it('should render page numbers as links in desired format', function () {
+            jsx.assertRender(PaginationBar, props,
+                '<li+><a href="/npm/npm/1"+>1</a></li><li+><a href="/npm/npm/2"+>2</a></li>' +
+                '<li+><a href="/npm/npm/3"+>3</a></li><li+><a href="/npm/npm/4"+>4</a></li>' +
+                '<li+><span+>5</span></li><li+><a href="/npm/npm/6"+>6</a></li>' +
+                '<li+><a href="/npm/npm/7"+>7</a></li><li+><a href="/npm/npm/8"+>8</a></li>' +
+                '<li+><a href="/npm/npm/9"+>9</a></li><li+><a href="/npm/npm/10"+>10</a></li>' +
+                '<li+><a href="/npm/npm/11"+>11</a></li><li+><a href="/npm/npm/12"+>12</a></li>' +
+                '<li+><a href="/npm/npm/13"+>13</a></li><li+><a href="/npm/npm/14"+>14</a></li>' +
+                '<li+><a href="/npm/npm/15"+>15</a></li>'
+            );
         });
     });
 });
