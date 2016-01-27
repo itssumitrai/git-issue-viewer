0d90ce370024f2b80ee4e5e199d6882f19675556
Sumit Rai <sumit.rai1987@gmail.com>
2016-01-26 23:32:58 -0800

    Add html test

--- tests/unit/components/Html.test.js
+++ tests/unit/components/Html.test.js
@@ -5,15 +5,67 @@
  */
 
 'use strict';
-import { expect } from 'chai';
 
-describe('stub', function () {
+import { expect } from 'chai';
+import { createMockComponentContext } from 'fluxible/utils';
+import ApplicationStore from '../../../stores/ApplicationStore';
+import RouteStore from '../../../stores/RouteStore';
+import Immutable from 'immutable';
+import jsx from 'jsx-test';
+
+describe('Html', function () {
+    let Html;
+
     before(function () {
+        Html = require('../../../components/Html');
     });
 
-    describe('#stub', function () {
-        it('should do something', function () {
-            expect(true).to.be.true;
+    describe('#render', function () {
+        let context;
+        let props;
+
+        beforeEach(function () {
+            context = createMockComponentContext({
+                stores: [ApplicationStore, RouteStore]
+            });
+
+            context.getStore(ApplicationStore).rehydrate({
+                pageTitle: 'This is the page title'
+            });
+
+            context.getStore(RouteStore)._currentRoute = Immutable.fromJS({
+                name: 'issueList',
+                title: 'List of all Issues'
+            });
+
+            props = {
+                markup: '<p>some markup</p>',
+                state: 'window.App={context: {}}',
+                clientFile: 'main.js',
+                context: context
+            };
+        });
+
+        it('should render html markup for the page', function () {
+            jsx.assertRender(Html, props,
+                '<html>' +
+                    '<head>' +
+                        '<meta charset="utf-8"/>' +
+                        '<title>This is the page title</title>' +
+                        '<meta name="viewport" content="width=device-width, user-scalable=no"/>' +
+                        '<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css"/>' +
+                        '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/octicons/3.3.0/octicons.min.css"/>' +
+                        '<link rel="stylesheet" href="/public/app.css"/>' +
+                    '</head>' +
+                    '<body>' +
+                        '<div id="app">' +
+                            '<p>some markup</p>' +
+                        '</div>' +
+                        '<script>window.App={context: {}}</script>' +
+                        '<script src="/public/main.js"></script>' +
+                    '</body>' +
+                '</html>'
+            );
         });
     });
 });
