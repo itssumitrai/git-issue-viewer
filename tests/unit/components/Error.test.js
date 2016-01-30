719004ee14703f9d09f4069debd5f42cae4df1dc
Sumit Rai <sumit.rai1987@gmail.com>
2016-01-29 20:35:19 -0800

    Add test for Error component

---
+++ tests/unit/components/Error.test.js
@@ -0,0 +1,61 @@
+/* globals expect, before, beforeEach, it, describe */
+/**
+ * Copyright 2016, Sumit Rai
+ * Copyrights licensed under the New MIT License. See the accompanying LICENSE file for terms.
+ */
+
+'use strict';
+
+import jsx from 'jsx-test';
+
+describe('Error', function () {
+    let ErrorComponent;
+
+    before(function () {
+        ErrorComponent = require('../../../components/Error');
+    });
+
+    describe('#render', function () {
+        let props;
+        beforeEach(function () {
+            props = {
+                error: {
+                    statusCode: 404,
+                    message: 'Not Found'
+                }
+            };
+        });
+
+        it('should render html tag and head', function () {
+            jsx.assertRender(ErrorComponent, props,
+                '<html>' +
+                    '<head>' +
+                        '<meta charset="utf-8"/>' +
+                        '<title>Oops! Error...</title>' +
+                        '<meta name="viewport" content="width=device-width, user-scalable=no"/>' +
+                        '<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css"/>' +
+                        '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/octicons/3.3.0/octicons.min.css"/>' +
+                        '<link rel="stylesheet" href="/public/app.css"/>' +
+                    '</head>'
+            );
+        });
+
+        it('should render error page', function () {
+            props.state = 'closed';
+            jsx.assertRender(ErrorComponent, props,
+                '<body>' +
+                    '<main+>' +
+                        '<section+>*' +
+                            '<span class="mega-octicon octicon-octoface icon"></span>*' +
+                            '<h2+>Oops, Encountered an Error</h2>' +
+                            '<div+>404</div>*' +
+                            '<div+>' +
+                                '<p>Not Found</p>' +
+                            '</div>' +
+                        '</section>' +
+                    '</main>' +
+                '</body>'
+            );
+        });
+    });
+});
